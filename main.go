16a84b52dbb87e41e3c47276045a75ed40d83c4d
Yuto2000 <urushimayuto@gmail.com>
2022-08-13 10:36:33 +0900

    ddsk

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-  fmt.Println("ddsk")
+  var counter int
+  for {
+    if ddsk() {
+      counter ++
+    } else {
+      counter = 0
+    }
+    if counter > 2 {
+      fmt.Print("ラブ注入!")
+      break
+    }
+  }
+}
+
+func ddsk() bool {
+  ddskStr := []string{"ドド", "スコ"}
+  success := []int{0, 1, 1, 1}
+  rand.Seed(time.Now().UnixNano())
+  var count int
+  for i:= 0; i < len(success); i++ {
+    rand := rand.Intn(2)
+    fmt.Print(ddskStr[rand])
+    if (rand == success[i]) {
+      count ++
+    }
+  }
+  return count > 3
 }
